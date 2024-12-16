af06c9ccd831dc6e781c73ad684a6a6c9325302b
ZeativeMedia <zaadevofc@gmail.com>
2024-12-16 11:52:39 +0700

    [UPDATE] vercel config

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,10 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'pages.main')->name('pages.main');
 
 Route::get('/auth/signin', [AuthController::class, 'showSignIn'])->name('auth.signin');
-Route::post('/auth/signin', [AuthController::class, 'signin']);
+Route::post('/auth/signin', [AuthController::class, 'signin'])->middleware('web');;
 
 Route::get('/auth/signup', [AuthController::class, 'showSignUp'])->name('auth.signup');
-Route::post('/auth/signup', [AuthController::class, 'signup']);
+Route::post('/auth/signup', [AuthController::class, 'signup'])->middleware('web');;
 
 Route::post('/auth/logout', [AuthController::class, 'signout'])->name('auth.signout');
 
